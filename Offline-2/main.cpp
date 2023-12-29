c8db0a5df53788f55a3b6bf5742eeba40fafbddc
Md. Nafiu Rahman <nafiu.rahman@gmail.com>
2023-12-29 17:26:59 +0600

    a

--- Offline-2/main.cpp
+++ Offline-2/main.cpp
@@ -1,5 +1,5 @@
 //main.cpp
-#include<iostream>
+#include<bits/stdc++.h>
 #include"Point.hpp"
 #include"Triangle.hpp"
 #include"Transform.hpp"
@@ -8,18 +8,37 @@ using namespace std;
 
 int main(){
     
+    ifstream in("scene.txt");
+    ofstream out("stage1.txt");
+
+    Point eye, look, up;
+    double fovY, aspectRatio, near, far;
+
+    //set eye.x eye.y eye.z from in
+    double x,y,z;
+    in>>x>>y>>z;
+    eye = Point(x,y,z);
+    //set look.x look.y look.z from in
+    in>>x>>y>>z;
+    look = Point(x,y,z);
+    //set up.x up.y up.z from in
+    in>>x>>y>>z;
+    up = Point(x,y,z);
+   
+    //set fovY from in
+    in>>fovY;
+    //set aspectRatio from in
+    in>>aspectRatio;
+    //set near from in
+    in>>near;
+    //set far from in
+    in>>far;
+
+    stack<Transform> st;
     Transform t;
-    t.translate(1,2,3);
-    t.print();
-    cout<<endl;
+    st.push(t);
 
-    Transform t2;
-    t2.scale(1,2,3);
-    t2.print();
+    // print top of stack
+    st.top().print();
 
-    cout<<endl;
-
-    //multiply t and t2 
-    Transform t3 = t*t2;
-    t3.print();
 }
